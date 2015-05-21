f0445b30098ca7493acd887bdc68ec40a550e62f
martin.haesler <martin.haesler@gmail.com>
2015-05-21 19:25:59 +0100

    set textview text to intent message text.

--- tut01/app/src/main/java/com/example/severin/myfirstall/DisplayMessageActivity.java
+++ tut01/app/src/main/java/com/example/severin/myfirstall/DisplayMessageActivity.java
@@ -1,6 +1,7 @@
 package com.example.severin.myfirstall;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends ActionBarActivity {
@@ -15,7 +17,11 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
