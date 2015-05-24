8ab62c02ae1dc85bcce5904a24fe3d975adf6c9b
martin.haesler <martin.haesler@gmail.com>
2015-05-24 07:14:31 +0100

    Add stubs for click listeners.

--- GeoQuiz/app/src/main/java/com/example/severin/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/severin/geoquiz/QuizActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -16,7 +17,19 @@ public class QuizActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
         mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            }
+            });
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void  onClick(View v) {
+
+            }
+        });
+
     }
 
     @Override
