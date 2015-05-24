974d2645e2adb6dd72992ce7781e417d9c450db0
martin.haesler <martin.haesler@gmail.com>
2015-05-24 07:29:31 +0100

    Add new class TrueFalse

---
+++ GeoQuiz/app/src/main/java/com/example/severin/geoquiz/TrueFalse.java
@@ -0,0 +1,15 @@
+package com.example.severin.geoquiz;
+
+/**
+ * Created by severin on 24/05/15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+}
