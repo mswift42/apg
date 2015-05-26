a78f50be8afe91e685f9834f63a393a1e11aa675
martin.haesler <martin.haesler@gmail.com>
2015-05-26 18:56:52 +0100

    Override onCreate method.

--- GeoQuiz/app/src/main/java/com/example/severin/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/severin/geoquiz/CheatActivity.java
@@ -1,9 +1,16 @@
 package com.example.severin.geoquiz;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by severin on 26/05/15.
  */
 public class CheatActivity  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
 }
