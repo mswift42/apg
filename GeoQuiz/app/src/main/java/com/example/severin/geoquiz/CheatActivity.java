6acbf03ef1fe178f6284a0480d441a87357337fe
martin.haesler <martin.haesler@gmail.com>
2015-05-26 19:36:42 +0100

    retrieve value from extra.

--- GeoQuiz/app/src/main/java/com/example/severin/geoquiz/CheatActivity.java
+++ GeoQuiz/app/src/main/java/com/example/severin/geoquiz/CheatActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 public class CheatActivity  extends Activity {
     public static final String EXTRA_ANSWER_IS_TRUE =
-            "com.severin.android.geoquiz.answer_is_true";
+            "com.example.severin.android.geoquiz.answer_is_true";
+
+    private boolean mAnswerIsTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
     }
 }
