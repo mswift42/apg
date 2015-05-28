b9976a6a32fcd181c4235ce722c0841ef901e3b1
martin.haesler <martin.haesler@gmail.com>
2015-05-28 18:15:38 +0100

    Add onCreateView method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -2,6 +2,9 @@ package mswift42.com.github.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
@@ -10,5 +13,12 @@ public class CrimeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+        return v;
     }
 }
