01d97a978b350a6ed5a12c63373171d29e10fd7a
martin.haesler <martin.haesler@gmail.com>
2015-05-28 18:13:10 +0100

    Override onCreate method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -1,6 +1,14 @@
 package mswift42.com.github.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
 }
