1712753abf0201ca82cfa426ecaf20fb40548b16
martin.haesler <martin.haesler@gmail.com>
2015-05-29 20:00:49 +0100

    Add fields mdate and msolved.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
@@ -1,10 +1,13 @@
 package mswift42.com.github.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
