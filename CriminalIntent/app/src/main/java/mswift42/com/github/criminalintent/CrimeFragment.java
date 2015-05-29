7d53cbe6096d4dfc8c342612449bb4e47733bc2d
martin.haesler <martin.haesler@gmail.com>
2015-05-29 20:22:16 +0100

    set datebutton text to mCrime date.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -45,6 +45,10 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
         return v;
     }
 }
