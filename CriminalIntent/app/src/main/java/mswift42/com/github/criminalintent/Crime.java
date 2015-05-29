3c9695a822d609fa2cc0bdb2df14c0bb2de659a5
martin.haesler <martin.haesler@gmail.com>
2015-05-29 20:03:39 +0100

    Add getters / setters for date and solved.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
@@ -9,6 +9,22 @@ public class Crime {
     private Date mDate;
     private boolean mSolved;
 
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
     public Crime() {
         mId = UUID.randomUUID();
         mDate = new Date();
