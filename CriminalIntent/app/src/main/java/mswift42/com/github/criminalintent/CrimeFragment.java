4e1621b2769ec3900a8af410f682ed4c710725f2
martin.haesler <martin.haesler@gmail.com>
2015-05-29 20:26:17 +0100

    Implement onCheckedChangeListener for checkbox.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
@@ -48,6 +49,14 @@ public class CrimeFragment extends Fragment {
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
 
         return v;
     }
