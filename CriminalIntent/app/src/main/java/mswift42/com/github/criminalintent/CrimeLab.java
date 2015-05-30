715a776d9aac3df2d26eb749708445934e9bcb45
martin.haesler <martin.haesler@gmail.com>
2015-05-30 13:19:26 +0100

    populate crimes with mock crime objects.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeLab.java
@@ -13,6 +13,12 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
+        for (int i = 0; i < 100; i++) {
+            Crime c = new Crime();
+            c.setTitle("Crime #" + i);
+            c.setSolved(i % 2 == 0);
+            mCrimes.add(c);
+        }
     }
 
     public static CrimeLab get(Context c) {
