d021b32cddc9b4aef1d40fd9701c59892d283d3f
martin.haesler <martin.haesler@gmail.com>
2015-05-30 16:34:08 +0100

    Rename activity_crime.xml to activity_fragment.xml.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeActivity.java
@@ -3,10 +3,7 @@ package mswift42.com.github.criminalintent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class CrimeActivity extends FragmentActivity {
@@ -14,7 +11,7 @@ public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
