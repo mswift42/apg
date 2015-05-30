e7d14ccba68edc23d6723ebf1c913237b13f3f11
martin.haesler <martin.haesler@gmail.com>
2015-05-30 19:44:13 +0100

    Override toString method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/Crime.java
@@ -38,6 +38,10 @@ public class Crime {
         mId = UUID.randomUUID();
         mDate = new Date();
     }
+    @Override
+    public String toString() {
+        return mTitle;
+    }
 
     public UUID getId() {
         return mId;
