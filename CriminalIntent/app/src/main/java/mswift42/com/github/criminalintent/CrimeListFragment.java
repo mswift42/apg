d4d749e55dae51abae8588daca6d488f83bd97bc
martin.haesler <martin.haesler@gmail.com>
2015-05-30 14:39:52 +0100

    add onCreate method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
@@ -1,4 +1,13 @@
 package mswift42.com.github.criminalintent;
 
-public class CrimeListFragment {
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class CrimeListFragment extends ListFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.crimes_title);
+    }
 }
