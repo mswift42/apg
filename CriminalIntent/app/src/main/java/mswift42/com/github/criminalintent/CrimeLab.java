d2f6809056e3ff85ba561eb50e8cf5b89442fb4f
martin.haesler <martin.haesler@gmail.com>
2015-05-30 12:19:01 +0100

    Setup singleton with private constructor.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeLab.java
@@ -1,7 +1,22 @@
 package mswift42.com.github.criminalintent;
 
+import android.content.Context;
+
 /**
  * Created by severin on 30/05/15.
  */
 public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+    private Context mAppContext;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+    }
+
+    public static CrimeLab get(Context c) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+        }
+        return sCrimeLab;
+    }
 }
