311f87626aba6f6596f915faefa5d09dbbf9c8bf
martin.haesler <martin.haesler@gmail.com>
2015-05-30 14:43:33 +0100

    Load crimelist

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
@@ -3,11 +3,15 @@ package mswift42.com.github.criminalintent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 
+import java.util.ArrayList;
+
 public class CrimeListFragment extends ListFragment {
+    private ArrayList<Crime> mCrimes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
+        mCrimes = CrimeLab.get(getActivity()).getCrimes();
     }
 }
