fa74800ae9ef30481c907473f09ede676daab31f
martin.haesler <martin.haesler@gmail.com>
2015-05-30 19:33:59 +0100

    setListAdaptor with mCrimes

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package mswift42.com.github.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -13,5 +14,10 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(getActivity(),
+                        android.R.layout.simple_list_item_1, mCrimes);
+        setListAdapter(adapter);
     }
 }
