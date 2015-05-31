efd288ea3b92960ba166b8284d57b1ba68e0e249
martin.haesler <martin.haesler@gmail.com>
2015-05-31 17:30:46 +0100

    Override onCreate method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
@@ -1,4 +1,14 @@
 package mswift42.com.github.criminalintent;
 
-public class CrimePagerActivity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
