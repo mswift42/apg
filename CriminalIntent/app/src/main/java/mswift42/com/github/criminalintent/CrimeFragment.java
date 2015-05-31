4cda63165f8dab8358629280776e543183bdeadf
martin.haesler <martin.haesler@gmail.com>
2015-05-31 14:17:55 +0100

    set each crime to matching title and solved status.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -36,6 +36,7 @@ public class CrimeFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
+        mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -57,6 +58,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
