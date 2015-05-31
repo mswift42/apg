43c3fdb8b9ac8487ef14e318473aad7b35cd47a6
martin.haesler <martin.haesler@gmail.com>
2015-05-31 08:48:30 +0100

    add clickHandler for listview.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeListFragment.java
@@ -2,12 +2,16 @@ package mswift42.com.github.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class CrimeListFragment extends ListFragment {
     private ArrayList<Crime> mCrimes;
+    private static final String TAG = "CrimeListFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,10 @@ public class CrimeListFragment extends ListFragment {
                 new ArrayAdapter<Crime>(getActivity(),
                         android.R.layout.simple_list_item_1, mCrimes);
         setListAdapter(adapter);
+    }
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Crime c = (Crime)(getListAdapter()).getItem(position);
+        Log.d(TAG, c.getTitle() + " was clicked");
     }
 }
