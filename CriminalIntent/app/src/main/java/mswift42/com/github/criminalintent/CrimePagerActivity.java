55b555cd82992b9bc8e1f0eed1fa45714579a56d
martin.haesler <martin.haesler@gmail.com>
2015-05-31 17:45:42 +0100

    add fragmentstatepageradapter.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
@@ -1,11 +1,17 @@
 package mswift42.com.github.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+
+import java.util.ArrayList;
 
 public class CrimePagerActivity extends FragmentActivity {
     private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,21 @@ public class CrimePagerActivity extends FragmentActivity {
         mViewPager = new ViewPager(this);
         mViewPager.setId(R.id.viewPager);
         setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
