9ee23d3d4be57bb6fcb589c6c4ca259446c136dc
martin.haesler <martin.haesler@gmail.com>
2015-06-01 17:21:34 +0100

    use Calandar to get year, month, day from mDate.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/DatePickerFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/DatePickerFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DatePickerFragment extends DialogFragment {
@@ -25,6 +26,13 @@ public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        mDate = (Date)getArguments().getSerializable(EXTRA_DATE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(mDate);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = getActivity().getLayoutInflater()
                 .inflate(R.layout.dialog_date, null);
         return new AlertDialog.Builder(getActivity())
