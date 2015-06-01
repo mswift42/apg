f9215cd47a3b38fa0e6ea958382ee926fa28bd98
martin.haesler <martin.haesler@gmail.com>
2015-06-01 16:55:52 +0100

    Add onClickListener to date button.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -1,5 +1,6 @@
 package mswift42.com.github.criminalintent;
 
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -21,6 +22,7 @@ public class CrimeFragment extends Fragment {
     private CheckBox mSolvedCheckBox;
     public static final String EXTRA_CRIME_ID =
             "mswift42.com.github.criminalintent.crime_id";
+    public static final String DIALOG_DATE = "date";
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -64,7 +66,15 @@ public class CrimeFragment extends Fragment {
         });
         mDateButton = (Button)v.findViewById(R.id.crime_date);
         mDateButton.setText(mCrime.formatDate());
-        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getActivity()
+                        .getSupportFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fm, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
