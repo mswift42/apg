a929f19ee5335be3e4797e7e436fb71b5d0320c5
martin.haesler <martin.haesler@gmail.com>
2015-06-01 17:42:16 +0100

    add onActivityResult method.

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimeFragment.java
@@ -1,5 +1,7 @@
 package mswift42.com.github.criminalintent;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +15,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class CrimeFragment extends Fragment {
@@ -90,5 +93,17 @@ public class CrimeFragment extends Fragment {
         });
 
         return v;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+       if (resultCode != Activity.RESULT_OK) {
+           return;
+       }
+        if (requestCode == REQUEST_DATE) {
+            Date date = (Date)data
+                    .getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date);
+            mDateButton.setText(mCrime.formatDate());
+        }
     }
 }
