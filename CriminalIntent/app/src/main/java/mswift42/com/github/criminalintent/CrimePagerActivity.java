a15935646cb200314c79867736b7470e4b3fcece
martin.haesler <martin.haesler@gmail.com>
2015-06-01 13:22:48 +0100

    Add string for date picker title

--- CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
+++ CriminalIntent/app/src/main/java/mswift42/com/github/criminalintent/CrimePagerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 
 import java.util.ArrayList;
 import java.util.UUID;
