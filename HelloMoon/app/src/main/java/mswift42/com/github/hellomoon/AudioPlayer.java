2d2109d72991a975f83736407ae3efdfb160ca46
martin.haesler <martin.haesler@gmail.com>
2015-06-02 20:33:32 +0100

    Add stop and play methods.

--- HelloMoon/app/src/main/java/mswift42/com/github/hellomoon/AudioPlayer.java
+++ HelloMoon/app/src/main/java/mswift42/com/github/hellomoon/AudioPlayer.java
@@ -1,4 +1,20 @@
 package mswift42.com.github.hellomoon;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 public class AudioPlayer {
+
+    private MediaPlayer mPlayer;
+
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+    public void play(Context c) {
+        mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+        mPlayer.start();
+    }
 }
