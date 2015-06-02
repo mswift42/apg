0b91441aa43d0555882d63ee13ea2df071c95e02
martin.haesler <martin.haesler@gmail.com>
2015-06-02 20:47:06 +0100

    before play stop a running mPlayer.

--- HelloMoon/app/src/main/java/mswift42/com/github/hellomoon/AudioPlayer.java
+++ HelloMoon/app/src/main/java/mswift42/com/github/hellomoon/AudioPlayer.java
@@ -14,7 +14,15 @@ public class AudioPlayer {
         }
     }
     public void play(Context c) {
+        stop();
         mPlayer = MediaPlayer.create(c, R.raw.one_small_step);
+
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
         mPlayer.start();
     }
 }
