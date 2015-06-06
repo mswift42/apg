ecbfb9cbd196a7f80af7fe1f908c9949e5bcd71e
martin.haesler <martin.haesler@gmail.com>
2015-06-06 15:18:01 +0100

    Initialize new adapter.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -28,6 +29,8 @@ public class MainActivityFragment extends Fragment {
         add("Fri - Foggy - 60 / 48");
         add("Sat - Sunny - 76 / 68");}
         };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview, mockStrings);
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
