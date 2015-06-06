0a365ea181f2756341e65927dbb5cc6ed5195602
martin.haesler <martin.haesler@gmail.com>
2015-06-06 15:09:03 +0100

    Add mock forecast strings.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/MainActivityFragment.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -18,6 +20,14 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayList<String> mockStrings = new ArrayList<String>() {{
+        add("Today - Sunny - 88 - 63");
+        add("Tomorrow - Foggy - 70 / 46");
+        add("Weds - Cloudy - 72 / 63");
+        add("Thurs - Rainy - 64 / 51");
+        add("Fri - Foggy - 60 / 48");
+        add("Sat - Sunny - 76 / 68");}
+        };
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
