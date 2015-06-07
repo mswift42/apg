fff126281abd4d9fc79d058e13809f901151805e
martin.haesler <martin.haesler@gmail.com>
2015-06-07 16:16:38 +0100

    Override onOptionsItemSelected method.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/ForecastFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -38,6 +39,14 @@ public class ForecastFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.forecastfragment, menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
