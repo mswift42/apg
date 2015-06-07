61c621fd620a508f2f8582404dce23c63b948624
martin.haesler <martin.haesler@gmail.com>
2015-06-07 10:30:12 +0100

    inflate menu in fragment, set hasOptionsmenu true.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/ForecastFragment.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/ForecastFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -25,6 +27,17 @@ import java.util.ArrayList;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecastfragment, menu);
     }
 
     @Override
