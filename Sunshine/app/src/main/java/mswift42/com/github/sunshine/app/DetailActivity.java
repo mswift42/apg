9387a9ba9e26a27ac3ba516526d568022d973671
mswift42 <martin.haesler@gmail.com>
2015-08-06 18:28:25 +0100

    add placeholdreFragment on create.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/DetailActivity.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/DetailActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.support.v4.app.Fragment;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -13,6 +13,11 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceHolderFragment())
+                    .commit();
+        }
     }
 
 
