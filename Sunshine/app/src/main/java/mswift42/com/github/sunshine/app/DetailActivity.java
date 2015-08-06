745dccbcdc030051603ad088bd1746640add4ea2
mswift42 <martin.haesler@gmail.com>
2015-08-06 18:35:11 +0100

    add static class PlaceHolderFragment.

--- Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/DetailActivity.java
+++ Sunshine/app/src/main/java/mswift42/com/github/sunshine/app/DetailActivity.java
@@ -3,9 +3,12 @@ package mswift42.com.github.sunshine.app;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -42,5 +45,16 @@ public class DetailActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static class PlaceHolderFragment extends Fragment {
+        public PlaceHolderFragment() {
+
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            return rootView;
+        }
     }
 }
