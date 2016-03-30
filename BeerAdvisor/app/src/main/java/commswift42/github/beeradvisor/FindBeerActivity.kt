50945378047c3ea99292d43c239931214fe77ac3
mswift42 <martin.haesler@gmail.com>
2016-03-30 19:22:09 +0100

    add java folder

---
+++ BeerAdvisor/app/src/main/java/commswift42/github/beeradvisor/FindBeerActivity.kt
@@ -0,0 +1,12 @@
+package commswift42.github.beeradvisor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FindBeerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_beer)
+    }
+}
