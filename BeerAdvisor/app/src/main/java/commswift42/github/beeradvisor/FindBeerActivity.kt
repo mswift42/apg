f1e0b184ba0c77afa98a2a1d4fb8ba0769a8acd8
mswift42 <martin.haesler@gmail.com>
2016-03-31 19:21:36 +0100

    Add onClick handler for button

--- BeerAdvisor/app/src/main/java/commswift42/github/beeradvisor/FindBeerActivity.kt
+++ BeerAdvisor/app/src/main/java/commswift42/github/beeradvisor/FindBeerActivity.kt
@@ -2,11 +2,17 @@ package commswift42.github.beeradvisor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_find_beer.*
+import kotlinx.android.synthetic.main.activity_find_beer.view.*
 
 class FindBeerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_beer)
+    }
+    fun onClickFindBeer(view: View):Unit {
+        brands.text = color.selectedItem.toString()
     }
 }
