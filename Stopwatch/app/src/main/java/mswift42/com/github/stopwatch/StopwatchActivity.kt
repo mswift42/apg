5b3fff5509d6d845596164d29bb50ccf5055d23e
mswift42 <martin.haesler@gmail.com>
2016-04-06 16:12:19 +0100

    add stopwatch app

---
+++ Stopwatch/app/src/main/java/mswift42/com/github/stopwatch/StopwatchActivity.kt
@@ -0,0 +1,12 @@
+package mswift42.com.github.stopwatch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StopwatchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stopwatch)
+    }
+}
