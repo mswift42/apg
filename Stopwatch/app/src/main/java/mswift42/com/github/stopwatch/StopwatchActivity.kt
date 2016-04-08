3268c29cc773ffea403b723baed3768fa9d6facd
mswift42 <martin.haesler@gmail.com>
2016-04-08 20:23:15 +0100

    Implement runTimer method

--- Stopwatch/app/src/main/java/mswift42/com/github/stopwatch/StopwatchActivity.kt
+++ Stopwatch/app/src/main/java/mswift42/com/github/stopwatch/StopwatchActivity.kt
@@ -2,11 +2,52 @@ package mswift42.com.github.stopwatch
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_stopwatch.*
 
 class StopwatchActivity : AppCompatActivity() {
+    private var seconds: Int = 0
+    private var running: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stopwatch)
+        runTimer()
     }
+    override fun onSaveInstanceState(savedInstanceState: Bundle?) {
+
+    }
+
+    fun onClickStart(view: View) {
+        running = true
+    }
+
+    fun onClickStop(view: View) {
+        running = false
+    }
+
+    fun onClickReset(view: View) {
+        running = false
+        seconds = 0
+    }
+
+    private fun runTimer(): Unit {
+        val handler = Handler()
+        val settimer = object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = (seconds % 3600) / 60
+                val sec = seconds % 60
+                val time = String.format("%d:%02d:%02d", hours, minutes, seconds)
+                watchtimer.text = time
+                if (running) {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+            }
+        }
+        handler.post(settimer)
+    }
+
 }
