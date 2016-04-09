ce5fe4a8464c5771e5775dadf79500c874a8687f
mswift42 <martin.haesler@gmail.com>
2016-04-09 11:08:38 +0100

    Add generated files

---
+++ Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/TopLevelActivity.kt
@@ -0,0 +1,12 @@
+package mswift42.com.github.starbuzz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TopLevelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_level)
+    }
+}
