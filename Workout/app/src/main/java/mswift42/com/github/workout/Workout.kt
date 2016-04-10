bcf139be620037dc5c30c30abab8d4e9f03d31df
mswift42 <martin.haesler@gmail.com>
2016-04-10 15:53:35 +0100

    Add class Workout

---
+++ Workout/app/src/main/java/mswift42/com/github/workout/Workout.kt
@@ -0,0 +1,21 @@
+package mswift42.com.github.workout
+
+class Workout private constructor(val name: String, val description: String) {
+
+    override fun toString(): String{
+        return name;
+    }
+    companion object {
+        val workouts = arrayOf(Workout("The Limb Loosener",
+                "5 Handstand push-ups\n10 1-legged squats\n15 Pull-ups"),
+                Workout("Core Agony",
+                        "100 Pull-ups\n100 Push-ups\n100 Squats"),
+                Workout("The Wimp Special",
+                        "5 Pull-ups\n10 Push-ups\n15 Squats"),
+                Workout("Strength and Length",
+                        "500 meter run\n21 x 1.5 pood kettleball swing\n21 x pull-ups"))
+    }
+}
+
+
+
