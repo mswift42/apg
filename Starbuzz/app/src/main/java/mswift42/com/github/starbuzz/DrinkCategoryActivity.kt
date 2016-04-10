92c97236f595e081f3046af8893be5f872826272
mswift42 <martin.haesler@gmail.com>
2016-04-10 09:53:17 +0100

    Don't load layout file

--- Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/DrinkCategoryActivity.kt
+++ Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/DrinkCategoryActivity.kt
@@ -3,11 +3,16 @@ package mswift42.com.github.starbuzz
 import android.app.ListActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class DrinkCategoryActivity : ListActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drink_category)
+        val listDrinks = listView
+
+        val listAdapter = ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks)
+
+        listDrinks.setAdapter(listAdapter)
     }
 }
