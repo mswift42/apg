119c64890806120dc25b39bdc6c504fdc48f6629
mswift42 <martin.haesler@gmail.com>
2016-04-10 10:13:49 +0100

    Implement onListItem Click listener

--- Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/DrinkCategoryActivity.kt
+++ Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/DrinkCategoryActivity.kt
@@ -1,9 +1,12 @@
 package mswift42.com.github.starbuzz
 
 import android.app.ListActivity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class DrinkCategoryActivity : ListActivity() {
 
@@ -14,5 +17,10 @@ class DrinkCategoryActivity : ListActivity() {
         val listAdapter = ArrayAdapter<Drink>(this, android.R.layout.simple_list_item_1, Drink.drinks)
 
         listDrinks.setAdapter(listAdapter)
+    }
+    public override fun onListItemClick(listView: ListView, itemView: View, position: Int, id: Long) {
+        val intent = Intent(this@DrinkCategoryActivity, DrinkActivity::class.java )
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, id as Int)
+        startActivity(intent)
     }
 }
