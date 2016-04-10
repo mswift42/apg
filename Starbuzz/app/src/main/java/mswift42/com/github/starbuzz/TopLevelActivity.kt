5cd9d233190d2b358db57c7d5c1ad0543912e44a
mswift42 <martin.haesler@gmail.com>
2016-04-10 09:10:59 +0100

    Add OnItemClickListener

--- Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/TopLevelActivity.kt
+++ Starbuzz/app/src/main/java/mswift42/com/github/starbuzz/TopLevelActivity.kt
@@ -1,12 +1,26 @@
 package mswift42.com.github.starbuzz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_top_level.*
 
 class TopLevelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_level)
+
+        val itemClickListener = AdapterView.OnItemClickListener{
+            listView, v: View, position: Int, id: Long ->
+            if (position == 0) {
+                val intent = Intent(this@TopLevelActivity, DrinkCategoryActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        list_options.setOnItemClickListener(itemClickListener)
     }
 }
