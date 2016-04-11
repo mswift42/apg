efd3b228ab7480831ae3e4424a5b03d9a6ec5752
mswift42 <martin.haesler@gmail.com>
2016-04-11 14:56:26 +0100

    Set text and description on start

--- Workout/app/src/main/java/mswift42/com/github/workout/WorkoutDetailFragment.kt
+++ Workout/app/src/main/java/mswift42/com/github/workout/WorkoutDetailFragment.kt
@@ -4,20 +4,37 @@ package mswift42.com.github.workout
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_workout_detail.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class WorkoutDetailFragment : Fragment() {
+    private var workoutId: Long = 0
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater!!.inflate(R.layout.fragment_workout_detail, container, false)
+    }
+    override fun onStart() {
+        super.onStart()
+        if (view != null) {
+            val title: TextView = textTitle as TextView
+            val workout = Workout.workouts[workoutId.toInt()]
+            title.text = workout.name
+            val desc: TextView = textDescription as TextView
+            desc.text = workout.description
+        }
+    }
+    public fun setWorkout(id: Long): Unit {
+        workoutId = id
     }
 
 }// Required empty public constructor
