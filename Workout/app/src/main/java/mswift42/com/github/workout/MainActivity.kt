dc40701c5ec71a921a2bbe2b5dcea42b119a4beb
mswift42 <martin.haesler@gmail.com>
2016-04-11 18:12:16 +0100

    Implement onItemClick method

--- Workout/app/src/main/java/mswift42/com/github/workout/MainActivity.kt
+++ Workout/app/src/main/java/mswift42/com/github/workout/MainActivity.kt
@@ -1,16 +1,25 @@
 package mswift42.com.github.workout
 
+import android.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WorkoutListFragment.WorkoutListListener  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val frag = supportFragmentManager.findFragmentById(R.id.detail_frag) as WorkoutDetailFragment
-        frag.setWorkout(1)
+    }
+
+    override fun itemClicked(id: Long) {
+        val details = WorkoutDetailFragment()
+        var ft = supportFragmentManager.beginTransaction()
+        details.setWorkout(id)
+        ft.replace(R.id.fragment_container, details)
+        ft.addToBackStack(null)
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+        ft.commit()
     }
 }
