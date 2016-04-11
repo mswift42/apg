a0be66241bade6f3bcc67e3f4b86dd6f665653c1
mswift42 <martin.haesler@gmail.com>
2016-04-11 16:12:03 +0100

    add kotlin file for list fragment

--- Workout/app/src/main/java/mswift42/com/github/workout/WorkoutListFragment.kt
+++ Workout/app/src/main/java/mswift42/com/github/workout/WorkoutListFragment.kt
@@ -3,22 +3,25 @@ package mswift42.com.github.workout
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.ListFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass.
  */
-class WorkoutListFragment : Fragment() {
+class WorkoutListFragment : ListFragment () {
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val textView = TextView(activity)
-        textView.setText(R.string.hello_blank_fragment)
-        return textView
+        val names = Workout.workouts.map { it.name }
+        val adapter = ArrayAdapter<String>(inflater!!.context, android.R.layout.simple_list_item_1,names)
+        setListAdapter(adapter)
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 
 }// Required empty public constructor
