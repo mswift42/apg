0b2eb152696ee97a8c935e4a92fbef90d0d11236
mswift42 <martin.haesler@gmail.com>
2016-04-11 15:46:56 +0100

    Add converted kotlin file

---
+++ Workout/app/src/main/java/mswift42/com/github/workout/WorkoutListFragment.kt
@@ -0,0 +1,24 @@
+package mswift42.com.github.workout
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class WorkoutListFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val textView = TextView(activity)
+        textView.setText(R.string.hello_blank_fragment)
+        return textView
+    }
+
+}// Required empty public constructor
