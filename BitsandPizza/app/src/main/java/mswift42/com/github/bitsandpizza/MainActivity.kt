bb6ae161ffe4415ea5f3a323b18fb8a6e48a4e55
mswift42 <martin.haesler@gmail.com>
2016-04-16 20:37:33 +0100

    Inflate menu options.

--- BitsandPizza/app/src/main/java/mswift42/com/github/bitsandpizza/MainActivity.kt
+++ BitsandPizza/app/src/main/java/mswift42/com/github/bitsandpizza/MainActivity.kt
@@ -2,11 +2,17 @@ package mswift42.com.github.bitsandpizza
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater?.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
