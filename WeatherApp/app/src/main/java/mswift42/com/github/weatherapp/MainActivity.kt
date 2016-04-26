65ccbdafac79440a13ae76fdcaa45986dc97c4e0
mswift42 <martin.haesler@gmail.com>
2016-04-26 18:56:17 +0100

    add sample weather items

---
+++ WeatherApp/app/src/main/java/mswift42/com/github/weatherapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package mswift42.com.github.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val forecastlist = findViewById(R.id.forecast_list) as RecyclerView
+        forecastlist.layoutManager = LinearLayoutManager(this)
+        forecastlist.adapter = ForecastListAdapter(items)
+    }
+}
